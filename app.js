cc22639b0ee8ea6a04eb97959a8e1052d5be5cef
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-01 14:55:17 -0500

    Fix setAttributes function

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ const shapes = [
         img: 'images/scissors.png'
     }
 ]
+// function to set the src of the image and an alt message for a11y
+function setAttributes(el, attrs) {
+    for (let key in attrs) {
+        el.setAttribute(key, attrs[key])
+    }
+}
 const computerShape = document.getElementById('computer')
 const computerPlay = () => {
     let choice = shapes[Math.floor(Math.random() * 3)]
-    computerShape.setAttribute('src', `${choice.img}`)
+    setAttributes(computerShape, {'src': `${choice.img}`, 'alt': "Chosen shape by the computer"})
 }
