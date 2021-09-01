ce9c8400183bf1b98b5c7a9003e6c3d3a4785c87
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-01 09:40:39 -0500

    Add function to get the chosen shape by the computer

---
+++ app.js
@@ -0,0 +1,19 @@
+const shapes = [
+    {
+        name: 'Rock',
+        img: 'images/rock.png'
+    },
+    {
+        name: 'Paper',
+        img: 'images/paper.png'
+    },
+    {
+        name: 'Scissors',
+        img: 'images/scissors.png'
+    }
+]
+const computerShape = document.getElementById('player')
+const computerPlay = () => {
+    let choice = shapes[Math.floor(Math.random() * 3)]
+    computerShape.setAttribute('src', `${choice.img}`)
+}
