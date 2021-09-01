1650427f4c59d7645e98b45dae3c15de9cac3158
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-01 09:52:51 -0500

    Fix computer shape id selector

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const shapes = [
         img: 'images/scissors.png'
     }
 ]
-const computerShape = document.getElementById('player')
+const computerShape = document.getElementById('computer')
 const computerPlay = () => {
     let choice = shapes[Math.floor(Math.random() * 3)]
     computerShape.setAttribute('src', `${choice.img}`)
