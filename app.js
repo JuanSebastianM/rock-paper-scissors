f70f063e170e66dfb29ea8354983ce74a1e34f50
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-02 15:17:04 -0500

    Add function to start playing. User can now click on Play to start

--- app.js
+++ app.js
@@ -18,8 +18,49 @@ function setAttributes(el, attrs) {
         el.setAttribute(key, attrs[key])
     }
 }
+// All Computer's information
 const computerShape = document.getElementById('computer')
+let compShapeName = document.getElementById('comp-shape')
 const computerPlay = () => {
     let choice = shapes[Math.floor(Math.random() * 3)]
     setAttributes(computerShape, {'src': `${choice.img}`, 'alt': "Chosen shape by the computer"})
+    compShapeName.textContent = `${choice.name}`
 }
+// All Player's information.
+const playerShape = document.getElementById('player')
+let playerShapeName = document.getElementById('player-shape')
+const playerPlay = () => {
+    let choice = shapes[Math.floor(Math.random() * 2)]
+    setAttributes(playerShape, {'src': `${choice.img}`, 'alt': "Chosen shape by the player"})
+    playerShapeName.textContent = `${choice.name}`
+}
+
+// Function to start playing upon click on button
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    playerPlay();
+    computerPlay();
+    if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Scissors') {
+        console.log('Rock beats scissors. Player gets +1!')
+    } 
+    else if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Rock') {
+        console.log('Rock beats scissors. Computer gets +1!')
+    }
+    if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Paper') {
+        console.log('Scissors beat paper. Player gets +1!')
+    }
+    else if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Scissors') {
+        console.log('Scissors beat paper. Computer gets +1!')
+    }
+    if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Rock') {
+        console.log('Paper beats rock. Player gets +1')
+    }
+    else if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Paper') {
+        console.log('Paper beats rock. Computer gets +1!')
+    }
+    if (playerShapeName.textContent === compShapeName.textContent) {
+        console.log('It is a draw. Keep going!')
+    }
+})
+//Now I have to create the score count variable to register every point.
+// And create the buttons so that the player can choose whatever he/she wants to play.
