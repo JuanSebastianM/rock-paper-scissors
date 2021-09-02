f8dd493ec40654cd8f2b465ac619c5508743e019
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-02 16:49:52 -0500

    Fix players count. Now it says whether the user has won or not once any of the players' score hits 5 point

--- app.js
+++ app.js
@@ -39,44 +39,59 @@ let playerScore = 0
 let playerLog = document.getElementById('player-score')
 let computerScore = 0
 let computerLog = document.getElementById('comp-score')
+let result = document.getElementById('result')
 // Function to start playing upon click on button
 const button = document.querySelector('button')
 button.addEventListener('click', () => {
     playerPlay();
     computerPlay();
-    if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Scissors') {
-        console.log('Rock beats scissors. Player gets +1!')
-        playerScore += 1
-        playerLog.textContent = playerScore
-    } 
-    else if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Rock') {
-        console.log('Rock beats scissors. Computer gets +1!')
-        computerScore += 1
-        computerLog.textContent = computerScore
-    }
-    if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Paper') {
-        console.log('Scissors beat paper. Player gets +1!')
-        playerScore += 1
-        playerLog.textContent = playerScore
-    }
-    else if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Scissors') {
-        console.log('Scissors beat paper. Computer gets +1!')
-        computerScore += 1
-        computerLog.textContent = computerScore
-    }
-    if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Rock') {
-        console.log('Paper beats rock. Player gets +1')
-        playerScore += 1
-        playerLog.textContent = playerScore
-    }
-    else if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Paper') {
-        console.log('Paper beats rock. Computer gets +1!')
-        computerScore += 1
-        computerLog.textContent = computerScore
-    }
-    if (playerShapeName.textContent === compShapeName.textContent) {
-        console.log('It is a draw. Keep going!')
-    }
-})
-//Now I have to create the score count variable to register every point.
-// And create the buttons so that the player can choose whatever he/she wants to play.
+        if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Scissors') {
+            result.textContent = 'Rock beats scissors. Player gets +1!'
+            playerScore += 1
+            playerLog.textContent = playerScore
+        } 
+        else if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Rock') {
+            result.textContent = 'Rock beats scissors. Computer gets +1!'
+            computerScore += 1
+            computerLog.textContent = computerScore
+        }
+        if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Paper') {
+            result.textContent = 'Scissors beat paper. Player gets +1!'
+            playerScore += 1
+            playerLog.textContent = playerScore
+        }
+        else if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Scissors') {
+            result.textContent = 'Scissors beat paper. Computer gets +1!'
+            computerScore += 1
+            computerLog.textContent = computerScore
+        }
+        if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Rock') {
+            result.textContent = 'Paper beats rock. Player gets +1'
+            playerScore += 1
+            playerLog.textContent = playerScore
+        }
+        else if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Paper') {
+            result.textContent = 'Paper beats rock. Computer gets +1!'
+            computerScore += 1
+            computerLog.textContent = computerScore
+        }
+        if (playerShapeName.textContent === compShapeName.textContent) {
+            result.textContent = 'It is a draw. Keep going!'
+        }
+        if (playerScore === 5) {
+            result.textContent = `You win! Congratulations!!`
+            playerScore = 0
+            playerLog.textContent = playerScore
+            computerScore = 0
+            computerLog.textContent = computerScore
+        } else if (computerScore === 5) {
+            result.textContent = `You lose :'(... Imagine losing to a machine, lol!`
+            playerScore = 0
+            playerLog.textContent = playerScore
+            computerScore = 0
+            computerLog.textContent = computerScore
+        }
+    })
+    // change button text to "play again" after having finished the round. move reset code snippets to the top.
+
+// Now create the buttons so that the player can choose whatever he/she wants to play.
