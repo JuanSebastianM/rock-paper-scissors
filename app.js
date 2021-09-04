504a8ebcd5ae510c1b300e34da4c427df3c2290e
Juan Sebastiáa Mendoza PPerez <cebo_052000@hotmail.com>
2021-09-04 16:56:15 -0500

    Fix starting score. Now it sets both players' score to 0 at the beginning

--- app.js
+++ app.js
@@ -26,14 +26,9 @@ const computerPlay = () => {
     setAttributes(computerShape, {'src': `${choice.img}`, 'alt': "Chosen shape by the computer"})
     compShapeName.textContent = `${choice.name}`
 }
-// All Player's information.
+// Player's information.
 const playerShape = document.getElementById('player')
 let playerShapeName = document.getElementById('player-shape')
-const playerPlay = () => {
-    let choice = shapes[Math.floor(Math.random() * 2)]
-    setAttributes(playerShape, {'src': `${choice.img}`, 'alt': "Chosen shape by the player"})
-    playerShapeName.textContent = `${choice.name}`
-}
 // Scores log
 let playerScore = 0
 let playerLog = document.getElementById('player-score')
@@ -42,69 +37,43 @@ let computerLog = document.getElementById('comp-score')
 let result = document.getElementById('result')
 // Function to start playing upon click on button
 const buttonPlay = document.querySelector('#button-play')
-// buttonPlay.addEventListener('click', () => {
-//     playerPlay();
-//     computerPlay();
-//         if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Scissors') {
-//             result.textContent = 'Rock beats scissors. Player gets +1!'
-//             playerScore += 1
-//             playerLog.textContent = playerScore
-//         } 
-//         else if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Rock') {
-//             result.textContent = 'Rock beats scissors. Computer gets +1!'
-//             computerScore += 1
-//             computerLog.textContent = computerScore
-//         }
-//         if (playerShapeName.textContent === 'Scissors' && compShapeName.textContent === 'Paper') {
-//             result.textContent = 'Scissors beat paper. Player gets +1!'
-//             playerScore += 1
-//             playerLog.textContent = playerScore
-//         }
-//         else if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Scissors') {
-//             result.textContent = 'Scissors beat paper. Computer gets +1!'
-//             computerScore += 1
-//             computerLog.textContent = computerScore
-//         }
-//         if (playerShapeName.textContent === 'Paper' && compShapeName.textContent === 'Rock') {
-//             result.textContent = 'Paper beats rock. Player gets +1'
-//             playerScore += 1
-//             playerLog.textContent = playerScore
-//         }
-//         else if (playerShapeName.textContent === 'Rock' && compShapeName.textContent === 'Paper') {
-//             result.textContent = 'Paper beats rock. Computer gets +1!'
-//             computerScore += 1
-//             computerLog.textContent = computerScore
-//         }
-//         if (playerShapeName.textContent === compShapeName.textContent) {
-//             result.textContent = 'It is a draw. Keep going!'
-//         }
-//         if (playerScore === 5) {
-//             result.textContent = `You win! Congratulations!!`
-//             playerScore = 0
-//             playerLog.textContent = playerScore
-//             computerScore = 0
-//             computerLog.textContent = computerScore
-//         } else if (computerScore === 5) {
-//             result.textContent = `You lose :'(... Imagine losing to a machine, lol!`
-//             playerScore = 0
-//             playerLog.textContent = playerScore
-//             computerScore = 0
-//             computerLog.textContent = computerScore
-//         }
-//     })
-    
-// change button text to "play again" after having finished the round. move reset code snippets to the top.
-
-
 //Hide #button-play after clicking on it and make .options-div visible. Then, after clicking on an option, hide that div and continue.
 const optionsDiv = document.querySelector('.options-div')
 buttonPlay.addEventListener('click', () => {
     optionsDiv.classList.toggle('options-visible')
     buttonPlay.classList.toggle('play-button-hidden')
+    if (result.textContent === '') {
+        playerScore = 0
+        playerLog.textContent = playerScore
+        computerScore = 0
+        computerLog.textContent = computerScore
+    }
 })
+// Functions to confirm whether it is a draw and if there's a winner
 function isADraw() { 
     if (playerShapeName.textContent === compShapeName.textContent) {
         result.textContent = 'It is a draw. Keep going!'
+    }
+}
+function checkWinner() {
+    if (playerScore === 5) {
+        result.textContent = `You win! Congratulations!!`
+        playerScore = 0
+        playerLog.textContent = playerScore
+        computerScore = 0
+        computerLog.textContent = computerScore
+        buttonPlay.addEventListener('click', () => {
+            result.textContent = ''
+        })
+    } else if (computerScore === 5) {
+        result.textContent = `You lose :'(... Imagine losing to a machine, lol!`
+        playerScore = 0
+        playerLog.textContent = playerScore
+        computerScore = 0
+        computerLog.textContent = computerScore
+        buttonPlay.addEventListener('click', () => {
+            result.textContent = ''
+        })
     }
 }
 // If Player plays rock
@@ -170,24 +139,3 @@ scissors.addEventListener('click', () => {
     buttonPlay.classList.toggle('play-button-hidden')
     optionsDiv.classList.toggle('options-visible')
 })
-function checkWinner() {
-    if (playerScore === 5) {
-        result.textContent = `You win! Congratulations!!`
-        playerScore = 0
-        playerLog.textContent = playerScore
-        computerScore = 0
-        computerLog.textContent = computerScore
-        buttonPlay.addEventListener('click', () => {
-            result.textContent = ''
-        })
-    } else if (computerScore === 5) {
-        result.textContent = `You lose :'(... Imagine losing to a machine, lol!`
-        playerScore = 0
-        playerLog.textContent = playerScore
-        computerScore = 0
-        computerLog.textContent = computerScore
-        buttonPlay.addEventListener('click', () => {
-            result.textContent = ''
-        })
-    }
-}
